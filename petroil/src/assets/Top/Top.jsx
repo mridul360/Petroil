29f8489932e094b6752f2a83ebfb85acd5e2357a
Mridul Hasan <125135774+mridul360@users.noreply.github.com>
2025-06-01 20:41:40 +0600

    icon

--- petroil/src/assets/Top/Top.jsx
+++ petroil/src/assets/Top/Top.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Container from '../Layout/Container'
+import { FiPhoneCall } from 'react-icons/fi'
 
 const Top = () => {
   return (
@@ -11,6 +12,7 @@ const Top = () => {
             <h1><a href=""></a>mail@yourcompany.com</h1>
           </div>
           <div>
+            <FiPhoneCall />
             <h1><a href="">+896 120 5889 (Toll free)</a></h1>
           </div>
         </div>
